1d64b8aaf4df72e5d91703001530a0700a6fae86
Andre <106348438+Andre-devel@users.noreply.github.com>
2025-09-10 22:56:09 -0300

    first commit

---
+++ src/test/java/br/com/andredevel/auth/service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andredevel.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
